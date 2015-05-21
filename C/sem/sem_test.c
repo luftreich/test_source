daf76d479133d552284882f8dccab7de2c08fe27
vincent <vincent@ubuntu-mac.(none)>
2015-05-21 12:53:31 +0800

    modify sem

--- C/sem/sem_test.c
+++ C/sem/sem_test.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/sem.h>
- 
+#include <pthread.h> 
 union semun
 {
     int val;
@@ -20,53 +20,63 @@ static int set_semvalue();
 static void del_semvalue();
 static int semaphore_p();
 static int semaphore_v();
- 
+
+static void * test_thread1(void * args)
+{
+	int i =0;
+	while (1)
+	{
+		/* 离开临界区，休眠随机时间后继续循环 */
+        if(!semaphore_p())
+        {
+			printf("exit line [%d]\n",__LINE__);
+            exit(EXIT_FAILURE);
+        }
+		i++;
+		printf("test thread %d \n",i);
+		sleep(1);
+	}
+}
 int main(int argc, char *argv[])
 {
-     char message = 'X';
-     int i = 0;
  
     /* 创建信号量 */
     sem_id = semget((key_t)1234, 1, 0666 | IPC_CREAT);
  
-    if(argc > 1)
-    {
         /* 程序第一次被调用，初始化信号量 */
         if(!set_semvalue())
         {
             fprintf(stderr, "Failed to initialize semaphore\n");
             exit(EXIT_FAILURE);
         }
-        /* 设置要输出到屏幕中的信息，即其参数的第一个字符 */
-        message = argv[1][0];
-        sleep(2);
-    }
 
-    for(i = 0; i < 10; ++i)
-    {
-         /* 进入临界区 */
-        if(!semaphore_p())
-        {
-            exit(EXIT_FAILURE);
-        }
-        /* 向屏幕中输出数据 */
-        printf("%c", message);
-        /* 清理缓冲区，然后休眠随机时间 */
-        fflush(stdout);
-        sleep(rand() % 3);
-        /* 离开临界区前再一次向屏幕输出数据 */
-        printf("%c", message);
-        fflush(stdout);
-        /* 离开临界区，休眠随机时间后继续循环 */
-        if(!semaphore_v())
-        {
-            exit(EXIT_FAILURE);
-        }
-        sleep(rand() % 2);
-    }
-    sleep(10);
-    printf("\n%d - finished\n", getpid());
- 
+	pthread_t p_tid = 0;
+	pthread_create(&p_tid,NULL,test_thread1,NULL);
+
+
+	while (1)
+	{
+		printf("please input a number : [1-10]\n");
+		char c = getchar();
+		int i = c-'0';
+		if (i<1)
+		{
+			continue;
+		}
+
+		printf("the number is %d\n",i);
+		int n = 0;
+		for (n = 0 ;n < i; n++)
+		{
+			/* 进入临界区 */
+			if(!semaphore_v())
+			{
+				printf("exit line [%d]\n",__LINE__);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+
     if(argc > 1)
     {
         /* 如果程序是第一次被调用，则在退出前删除信号量 */
@@ -81,7 +91,7 @@ static int set_semvalue()
     /* 用于初始化信号量，在使用信号量前必须这样做 */
     union semun sem_union;
  
-    sem_union.val = 1;
+    sem_union.val = 0;
     if(semctl(sem_id, 0, SETVAL, sem_union) == -1)
     {
         return 0;
